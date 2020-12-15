2c4268ed54ccd3186175b434506fcf7c1ccfaa7a
Sampath Basnagoda <sampath@huubap.com>
2020-12-15 08:06:24 +0530

    Initial Commit

---
+++ ScrollPagingZoomingTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollPagingZoomingTest
+//
+//  Created by Sampath Basnagoda on 12/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
